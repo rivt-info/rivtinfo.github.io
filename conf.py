a3250514c34c92cc88d19990e9fb3536ddc2e7c0
rholland <rholland@structurelabs.com>
2023-09-23 00:26:14 -0700

    update 78 files

--- conf.py
+++ conf.py
@@ -33,7 +33,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'insegel'
 html_static_path = ['_static', ]
-html_logo_url = "_static/riv01a.png"
 html_theme_options = {
     'navigation_depth': 2
 }
