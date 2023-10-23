cf5dda09950424415e4dafe4efd9305ee2e58d91
rholland <rholland@structurelabs.com>
2023-10-23 16:07:12 -0700

    update 96 files

--- conf.py
+++ conf.py
@@ -1,31 +1,12 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 # -- Path setup --------------------------------------------------------------
-import os
 import sys
 from pathlib import Path
-from typing import Any, Dict
-
-import pydata_sphinx_theme
-from sphinx.application import Sphinx
-
 sys.path.append(str(Path(".").resolve()))
-
-# -- Project information -----------------------------------------------------
 
 project = 'rivt'
 copyright = '2023 StructureLabs'
 author = 'rholland'
 release = '0.1'
-
-
-# -- General configuration ---------------------------------------------------
 
 myst_heading_anchors = 3
 
@@ -40,19 +21,18 @@ source_suffix = ['.rst', '.md']
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
 html_theme = 'pydata_sphinx_theme'
 html_static_path = ['_static', ]
 html_logo = "_static/img/rivt01a.png"
 html_theme_options = {
     "show_nav_level": 2,
     "show_toc_level": 3,
+    "navigation_depth": 3,
     "navbar_align": "content",
     "navbar_start": ["navbar-logo"],
     "navbar_center": ["navbar-nav"],
     "navbar_end": ["navbar-icon-links"],
     "navbar_persistent": ["search-button"],
-    "navigation_depth": 3,
     "logo": {
         "text": "rivt",
         "image_dark": "_static/img/riv-dark8.png",
@@ -62,6 +42,13 @@ html_theme_options = {
 html_sidebars = {
     "**": ["sidebar-nav-bs"]
 }
+
+
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
