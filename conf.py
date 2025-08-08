789aec1668fccd6b360c6fe2c4f539fcab130cac
rholland <rholland@structurelabs.com>
2025-08-07 21:43:36 -0700

    image update

--- conf.py
+++ conf.py
@@ -29,8 +29,8 @@ html_theme_options = {
     "footer_start": ["copyright"],
     "footer_end": [],
     "logo": {
-        "image_dark": "_static/img/rivhome11d.png",
-        "image_light": "_static/img/rivhome11d.png",
+        "image_dark": "_static/img/rivhome11c.png",
+        "image_light": "_static/img/rivhome11c.png",
     }
 }
 
