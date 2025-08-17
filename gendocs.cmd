4844095cdb18fba586ad9da7f230843e3cc450ce
rholland <rholland@structurelabs.com>
2025-08-17 15:46:51 -0700

    Merge branch 'main' of https://github.com/rivt-info/rivtinfo.github.io

--- gendocs.cmd
+++ gendocs.cmd
@@ -1,7 +1,7 @@
 echo Y | rmdir docs /S
 sphinx-build rstdocs docs -E
-echo F | xcopy /y CNAME .\docs\CNAME
-echo F | xcopy /y .nojekyll .\docs\.nojekyll
-echo F | xcopy /y .\rstdocs\README.txt .\docs\README.txt
+copy /y CNAME .\docs\
+copy /y .nojekyll .\docs\
+xcopy /y .\rstdocs\README.txt .\docs\
 
 
