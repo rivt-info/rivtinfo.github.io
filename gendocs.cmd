f758bc6092f294d3c44516418a0fcfc9b4972b1a
rholland <rholland@structurelabs.com>
2025-08-17 15:29:20 -0700

    site refactor

--- gendocs.cmd
+++ gendocs.cmd
@@ -2,6 +2,6 @@ echo Y | rmdir docs /S
 sphinx-build rstdocs docs -E
 echo F | scopy /y CNAME .\docs\CNAME
 echo F | scopy /y .nojekyll .\docs\.nojekyll
-echo F | scopy /y README.rst .\docs\README.rst
+echo F | scopy /y .\rstdocs\README.txt .\docs\README.txt
 
 
