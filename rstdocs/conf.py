a1600a839a487cc387a8c35ba311b1b328a7fcdd
rholland <rholland@structurelabs.com>
2025-08-30 19:47:07 -0700

    format changes

--- rstdocs/conf.py
+++ rstdocs/conf.py
@@ -31,7 +31,7 @@ html_static_path = ['_static', '_static/img']
 html_css_files = ['css/custom.css',]
 html_theme_options = {
     "pygment_light_style": "tango",  
-    "pygment_dark_style": "monokai",   
+    "pygment_dark_style": "github-dark",   
     "navbar_start": ["navbar-logo", "index_link"],
     "collapse_navigation": True ,
     "header_links_before_dropdown": 6,
--- rstdocs/dv03-markup/example1.rst
+++ rstdocs/dv03-markup/example1.rst
@@ -1,7 +1,7 @@
 **3.4** Example 1
 ===================
 
-simple example 2
+simple example 3
 
 
 .. include:: examples/example1.rst
