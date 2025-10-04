e68e2afcf3528a57dbef318e0e88ffef64ebcdc7
rholland <rholland@structurelabs.com>
2025-10-04 13:51:14 -0700

    complete the docs

---
+++ docs/_downloads/fbba7843191b5c636109314de90fb96e/rivt-uv.cmd
@@ -0,0 +1,32 @@
+@echo on
+REM (1) clear any existing project, (2) create new venv
+REM (3) install rivt, (4) download example
+REM set rivt folder
+SET rvfolder=rivt-doc1
+REM go to home directory
+cd %HOMEPATH%
+REM double check deactivation
+uv deactivate
+REM double check that old project is deleted
+rmdir /s /q %rvfolder%
+REM (1) clean cache 
+uv cache clean
+REM set up venv
+mkdir %rvfolder%
+REM change directory
+cd %rvfolder%
+REM download rivt install file
+curl  https://raw.githubusercontent.com/rivt-info/rivt-win-install/refs/heads/main/rivt-install.cmd -O   
+REM (2) make venv
+uv venv
+REM activate venv
+call .venv/scripts/activate
+REM (3) install rivt from GitHub
+uv pip install git+https://github.com/rivtlib-dev/rivtlib.git@main
+REM (4) download example project into new folder
+mkdir example1
+cd example1
+curl https://raw.githubusercontent.com/rivt-info/rivt-simple-single-doc/refs/heads/main/rv0000-simple-single-doc.py -O
+curl https://github.com/rivt-info/rivt-simple-single-doc/blob/main/beam.png?raw=true -O -L
+REM run example with (no quotes) "python rv0000-simple-single-doc.py"
+cmd /k
