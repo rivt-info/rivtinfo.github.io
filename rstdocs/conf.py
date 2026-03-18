39c6ceddeb8465e6b732855420169c9d2c32c1a9
rholland <rholland@structurelabs.com>
2026-03-17 17:54:16 -0700

    complete4

--- rstdocs/conf.py
+++ rstdocs/conf.py
@@ -1,7 +1,15 @@
 import sys
+import os
 from pathlib import Path
 sys.path.append(str(Path(".").resolve()))
-
+pypathS = os.path.dirname(sys.executable)
+rvstyleP = os.path.join(
+    pypathS,
+    "Lib",
+    "site-packages",
+    "rivtlib",
+    "styles",
+)
 project = 'rivt.info'
 copyright = '2023 StructureLabs LLC'
 author = 'rholland'
@@ -11,7 +19,8 @@ extensions = ['sphinx.ext.githubpages','sphinx_togglebutton',
               "sphinxcontrib.jquery", 'sphinx_copybutton',
               'sphinx_favicon', 'sphinx.ext.duration',
               'sphinx.ext.doctest', 'sphinx.ext.autodoc',
-              'sphinx_design','sphinx_new_tab_link','rst2pdf.pdfbuilder']
+              'sphinx_design','sphinx_new_tab_link',
+              'rst2pdf.pdfbuilder']
 master_doc = 'index'
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 source_suffix = ['.rst', '.md']
@@ -55,61 +64,51 @@ favicons = [
 
 ]
 
-
 # -- Options for PDF output -------------------------------------------------
-# Grouping the document tree into PDF files. List of tuples
-# (source start file, target name, title, author, options).
-#
-# If there is more than one author, separate them with \\.
-# For example: r'Guido van Rossum\\Fred L. Drake, Jr., editor'
-#
+# List of tuples  (source start file, target name, title, author, options).
 # The options element is a dictionary that lets you override
 # this config per-document. For example:
-#
 # ('index', 'MyProject', 'My Project', 'Author Name', {'pdf_compressed': True})
+# More than one author, separate them with \\.
+# r'Guido van Rossum\\Fred L. Drake, Jr., editor'
 #
-# would mean that specific document would be compressed
-# regardless of the global 'pdf_compressed' setting.
 pdf_documents = [
-    ('index', 'MyProject', 'My Project', 'Author Name'),
+    ('index', 'rivt_manual', 'rivt', 'r holland'),
 ]
-# A comma-separated list of custom stylesheets. Example:
-pdf_stylesheets = ['sphinx', 'a4']
+# Label to use as a prefix for the subtitle on the cover page
+subtitle_prefix = 'User Manual'
 # A list of folders to search for stylesheets. Example:
-pdf_style_path = ['.', '_styles']
-# Create a compressed PDF
-# Use True/False or 1/0
-# Example: compressed=True
-# pdf_compressed = False
+pdf_style_path = ['./pdfdocs/stylepdf']
 # A colon-separated list of folders to search for fonts. Example:
-# pdf_font_path = ['/usr/share/fonts', '/usr/share/texmf-dist/fonts/']
+pdf_font_path = ['./pdfdocs/fontpdf']
+# A comma-separated list of custom stylesheets. Example:
+pdf_stylesheets = ['stylepdf1.yaml']
+# Example: compressed=True
+pdf_compressed = False
 # Language to be used for hyphenation support
-# pdf_language = "en_US"
-# Mode for literal blocks wider than the frame. Can be
-# overflow, shrink or truncate
-# pdf_fit_mode = "shrink"
-# Section level that forces a break page.
-# For example: 1 means top-level sections start in a new page
-# 0 means disabled
-# pdf_break_level = 0
+pdf_language = "en_US"
+# literal blocks wider than the frame overflow, shrink or truncate
+pdf_fit_mode = "shrink"
+# 1 means top-level sections start in a new page 0 disabled
+pdf_break_level = 0
 # When a section starts in a new page, force it to be 'even', 'odd',
 # or just use 'any'
-# pdf_breakside = 'any'
+pdf_breakside = 'any'
 # Insert footnotes where they are defined instead of
 # at the end.
-# pdf_inline_footnotes = True
+pdf_inline_footnotes = True
 # verbosity level. 0 1 or 2
 # pdf_verbosity = 0
 # If false, no index is generated.
-# pdf_use_index = True
+pdf_use_index = True
 # If false, no modindex is generated.
-# pdf_use_modindex = True
+pdf_use_modindex = True
 # If false, no coverpage is generated.
-# pdf_use_coverpage = True
+pdf_use_coverpage = True
 # Name of the cover page template to use
 # pdf_cover_template = 'sphinxcover.tmpl'
-# Label to use as a prefix for the subtitle on the cover page
-# subtitle_prefix = 'version'
+# Page template name for "regular" pages
+# pdf_page_template = 'cutePage'
 # Documents to append as an appendix to all manuals.
 # pdf_appendices = []
 # Enable experimental feature to split table cells. Use it
@@ -119,10 +118,8 @@ pdf_style_path = ['.', '_styles']
 # pdf_default_dpi = 72
 # Enable rst2pdf extension modules
 # pdf_extensions = []
-# Page template name for "regular" pages
-# pdf_page_template = 'cutePage'
 # Show Table Of Contents at the beginning?
-# pdf_use_toc = True
+pdf_use_toc = True
 # How many levels deep should the table of contents be?
 pdf_toc_depth = 9999
 # Add section number to section references
